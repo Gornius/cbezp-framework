df94baeaf59a494f8204fe60797cb0a3d7defae1
Rafał Cisek <rafal.cisek121@gmail.com>
2022-03-27 22:04:44 +0200

    Update Post model

--- mvc/models/Post.php
+++ mvc/models/Post.php
@@ -4,4 +4,22 @@ include_once 'classes/Model.php';
 
 class Post extends Model {
     public $table_name = 'post';
+    public $fields = [
+        'name' => [
+            'db_type' => 'varchar(255)',
+        ],
+        'type' => [
+            'db_type' => 'varchar(255)',
+        ],
+        'name' => [
+            'db_type' => 'varchar(255)',
+        ],
+        'message' => [
+            'db_type' => 'varchar(255)',
+        ],
+        'deleted' => [
+            'db_type' => 'boolean',
+            'default' => 0,
+        ]
+    ];
 }
