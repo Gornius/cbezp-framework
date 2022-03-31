59947b2f29b8a4058fb3b0925baf360502f0b2bf
Rafał Cisek <rafal.cisek121@gmail.com>
2022-03-31 21:52:31 +0200

    Add unoptimized mysqlPDO implementation

--- classes/Dependencies.php
+++ classes/Dependencies.php
@@ -1,11 +1,12 @@
 <?php
 include_once 'interfaces/IDatabase.php';
 include_once 'classes/mysqliDatabase.php';
+include_once 'classes/mysqlPDODatabase.php';
 include_once 'classes/SmartyWrapper.php';
 
 class Dependencies {
     static function get_database() :IDatabase {
-        return new mysqliDatabase();
+        return new mysqlPDODatabase();
     }
     static function get_smarty() :Smarty {
         return new SmartyWrapper();
