ff3f2bf4daf0c6965410b57bb98935ecbd86c078
Rafał Cisek <rafal.cisek121@gmail.com>
2022-05-02 01:44:14 +0200

    Change way of verifying admin in admin controller

--- classes/Controller.php
+++ classes/Controller.php
@@ -3,4 +3,13 @@
 class Controller {
     public function check_access($permission) {
     }
+
+    public function check_super_admin() {
+        $user = $_SESSION['user'];
+        if (!$user['super_admin']) {
+            echo 'Only super admin can access this resource';
+            return false;
+        }
+        return true;
+    }
 }
