5631d85464e5e2344fc9f42dd898f7419ee739a5
Rafał Cisek <rafal.cisek121@gmail.com>
2022-05-01 19:50:10 +0200

    Initiate permissions table

--- mvc/models/Permission.php
+++ mvc/models/Permission.php
@@ -9,4 +9,24 @@ class Permission extends Model {
             'db_type' => 'varchar(255)',
         ],
     ];
+
+    public function init() {
+        $hardcoded_permissions = [
+            'add_post',
+            'remove_any_post',
+            'remove_own_post',
+            'edit_any_post',
+            'edit_own_post',
+            'view_public_posts',
+            'view_own_private_posts',
+            'view_all_posts',
+        ];
+
+        foreach($hardcoded_permissions as $permission) {
+            $record = [
+                "name" => $permission,
+            ];
+            $this->save($record);
+        }
+    }
 }
