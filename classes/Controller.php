730086dc828ab8a41a24d5a2324607c70d4082e8
Rafał Cisek <rafal.cisek121@gmail.com>
2022-05-02 02:06:40 +0200

    Cosmetic fix

--- classes/Controller.php
+++ classes/Controller.php
@@ -7,7 +7,6 @@ class Controller {
     public function check_super_admin() {
         $user = $_SESSION['user'];
         if (!$user['super_admin']) {
-            echo 'Only super admin can access this resource';
             return false;
         }
         return true;
